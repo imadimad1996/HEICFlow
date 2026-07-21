9e065bbf524814b7b136b5f6e096806612a44821
imadimad1996 <imadazizimad@gmail.com>
2026-07-21 01:04:43 +0100

    feat: add InlineNativeAd widget and update Play Console store metadata in multiple languages

--- lib/widgets/inline_native_ad.dart
+++ lib/widgets/inline_native_ad.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 
-import '../features/settings/settings_controller.dart';
 import '../utils/constants.dart';
 
 enum InlineNativeAdSize { small, medium }
